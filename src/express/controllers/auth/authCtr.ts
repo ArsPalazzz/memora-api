1d8f55d049566308916f83952bc586dee5a2e105
Arseniy <palaznikarseni@gmail.com>
2025-12-28 02:30:00 +0300

    fix build

--- src/express/controllers/auth/authCtr.ts
+++ src/express/controllers/auth/authCtr.ts
@@ -25,7 +25,7 @@ export async function authCtr(req: Request, res: Response, next: NextFunction) {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
