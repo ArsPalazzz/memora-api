414b06a915fd9b1c52c46afa61c0e5438f4f8f9e
Arseniy <palaznikarseni@gmail.com>
2026-06-21 00:45:03 +0300

    Remove dist

--- dist/express/routers/healthCheck.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.healthCheck = void 0;
-const express_1 = require("express");
-const healthCheckCtr_1 = require("../controllers/healthCheck/healthCheckCtr");
-const healthCheck = (0, express_1.Router)();
-exports.healthCheck = healthCheck;
-healthCheck.get('/health', healthCheckCtr_1.getHealthCtr);
-healthCheck.use('/v3/api-docs', healthCheckCtr_1.sendSwaggerDocCtr);
